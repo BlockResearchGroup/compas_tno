cf8147c14063aac9bc8189566e63d243f8c8181a
tomvanmele <tom.v.mele@gmail.com>
2024-12-05 22:51:18 +0100

    crossvault minmax viewer

--- examples/_tutorial/2_crossvault_minmax.py
+++ examples/_tutorial/2_crossvault_minmax.py
@@ -1,3 +1,9 @@
+import math
+
+from compas_viewer import Viewer
+
+from compas.geometry import Cylinder
+from compas.colors import Color
 from compas_tno.analysis import Analysis
 from compas_tno.diagrams import FormDiagram
 from compas_tno.shapes import Shape
@@ -40,3 +46,34 @@ analysis.run()
 
 # view = Viewer(form)
 # view.show_solution()
+
+# =============================================================================
+# Viz
+# =============================================================================
+
+viewer = Viewer()
+
+viewer.scene.add(vault.middle, show_lines=False, name="Middle")
+viewer.scene.add(vault.intrados, show_lines=False, name="Intrados", opacity=0.5)
+viewer.scene.add(vault.extrados, show_lines=False, name="Extrados", opacity=0.5)
+
+edges = list(form.edges_where({"_is_edge": True}))
+
+max_thick = 0.1
+forces = [form.edge_attribute(edge, "q") * form.edge_length(edge) for edge in edges]
+fmax = math.sqrt(max(abs(max(forces)), abs(min(forces))))
+
+pipes = []
+for edge in edges:
+    q = form.edge_attribute(edge, "q")
+    line = form.edge_line(edge)
+    length = line.length
+    force = math.sqrt(abs(q * length))
+    radius = force / fmax * max_thick
+    pipe = Cylinder.from_line_and_radius(line, radius)
+    if force > 1e-3:
+        pipes.append(pipe)
+
+viewer.scene.add(pipes, name="Pipes", color=Color.red())
+
+viewer.show()
